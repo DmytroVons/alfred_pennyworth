87c82b9a819098e9e1e29b21760b0bab43b0ef12
Dmytro Vons <dmytrovons@gmail.com>
2020-11-19 23:52:28 +0200

    changes v3

--- ArgumentsParsing/homework/pars_1.py
+++ ArgumentsParsing/homework/pars_1.py
@@ -18,9 +18,9 @@ def merge_files(source_files, path_to_source, destination_filename, path_to_dest
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process command line arguments.')
-    parser.add_argument('-sy', '--start_year', required=False, default=min(os.listdir('beer_review/'))[:4], dest='start_year')
-    parser.add_argument('-ey', '--end_year', required=False, default=max(os.listdir('beer_review/'))[:4], dest='end_year')
-    parser.add_argument('-pf', '--path_to_source_files', required=True, dest='path_to_source_files')
+    parser.add_argument('-sy', '--start_year', required=False, default=1998, dest='start_year')
+    parser.add_argument('-ey', '--end_year', required=False, default=2008, dest='end_year')
+    parser.add_argument('-pf', '--path_to_source_files', required=True, default='.', dest='path_to_source_files')
     parser.add_argument('-dp', '--destination_path', required=False, default='.', dest='destination_path')
     parser.add_argument('-df', '--destination_filename', required=False, default='.', dest='destination_filename')
     return parser.parse_args()
@@ -28,6 +28,6 @@ def parse_arguments():
 
 if __name__ == "__main__":
     arguments = parse_arguments()
-    if arguments.path_to_source_file:
+    if arguments.path_to_source_files:
         files = file_names(arguments.start_year, arguments.end_year, arguments.path_to_source_files)
         merge_files(files, arguments.path_to_source_files, arguments.destination_filename, arguments.destination_path)
