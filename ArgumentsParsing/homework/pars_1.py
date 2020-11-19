df81e6f42a276b9b3349cdf503becb9d05dcf9ee
Dmytro Vons <dmytrovons@gmail.com>
2020-11-19 19:58:48 +0200

    my brain out

--- ArgumentsParsing/homework/pars_1.py
+++ ArgumentsParsing/homework/pars_1.py
@@ -22,11 +22,12 @@ def parse_arguments():
     parser.add_argument('-ey', '--end_year', required=False, default=2008, dest='end_year')
     parser.add_argument('-pf', '--path_to_source_files', required=True, dest='path_to_source_files')
     parser.add_argument('-dp', '--destination_path', required=False, default='.', dest='destination_path')
-    parser.add_argument('-df', '--destination_filename', required=False, default=' ', dest='destination_filename')
+    parser.add_argument('-df', '--destination_filename', required=False, default='.', dest='destination_filename')
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     arguments = parse_arguments()
-    files = file_names(arguments.start_year, arguments.end_year, arguments.path_to_source_files)
-    merge_files(files, arguments.path_to_source_files, arguments.destination_filename, arguments.destination_path)
+    if arguments.path_to_source_file:
+        files = file_names(arguments.start_year, arguments.end_year, arguments.path_to_source_files)
+        merge_files(files, arguments.path_to_source_files, arguments.destination_filename, arguments.destination_path)
